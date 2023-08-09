2ff311f2cb1a7fe1ee1bca85bf7a24ec03097f38
Bryson Cale <bryson.cale1@gmail.com>
2023-08-09 09:40:41 -0700

    Updates for nelder mead solver

--- src/fitting.jl
+++ src/fitting.jl
@@ -21,15 +21,19 @@ Perform a maximum a posteriori fit using IterativeNelderMead.jl. Returns a Named
 """
 function run_mapfit(post::RVPosterior, p0::Parameters; output_path=nothing)
     println("Running MAP fit ...")
-    vecs = to_vecs(p0)
+    p0_vecs = to_vecs(p0)
     ptest = deepcopy(p0)
     pbest = deepcopy(p0)
+    scale_factors = [par.vary ? get_scale(par) : 0 for par in values(p0)]
     obj_wrapper = (x) -> begin
         set_values!(ptest, x)
         return -1 * compute_logaprob(post, ptest)
     end
-    map_result = (;pbest=pbest, lnL=NaN, fcalls=0, simplex=nothing, iteration=0)
-    opt_result = IterativeNelderMead.optimize(obj_wrapper, vecs.values, vary=vecs.vary)
+    opt_result = IterativeNelderMead.optimize(obj_wrapper, p0_vecs.values;
+                        vary=p0_vecs.vary,
+                        scale_factors,
+                        options=(;ftol_rel=1E-8)
+                    )
     set_values!(pbest, opt_result.pbest)
     map_result = (;pbest=pbest, lnL=-1 * opt_result.fbest, fcalls=opt_result.fcalls, simplex=opt_result.simplex, iteration=opt_result.iterations)
     println("Completed MAP fit")
